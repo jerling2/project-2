9ba6f869f1b0ae40292d035873a2a389b9c7a2b2
Sterling Stewart <sterling.silver123@gmail.com>
2023-05-23 23:52:17 -0700

    skeleton pages up

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import index
+from .views import index, schedulebuilder, professors, courses, degreereqs
 
 # >>>>>>>>>>>>>>>>>>>>>>>>> TEMPORARY TEST >>>>>>>>>>>>>>>>>>>>>>>>>
 from django.http import HttpResponse
@@ -35,7 +35,11 @@ def foobar(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', foobar),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('schedulebuilder/', schedulebuilder, name='schedulebuilder'),
+    path('professors/', professors, name='professors'),
+    path('courses/', courses, name='courses'),
+    path('degreereqs/', degreereqs, name='degreereqs'),
 ] + static(settings.STATIC_URL)
 
 
