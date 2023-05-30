8ddadab19783328bfa1a54991ecde02fe4705f06
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-05-29 18:01:50 -0700

    Integrating websockets and events

--- project/asgi.py
+++ project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project project.
+ASGI config for project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.routing import ProtocolTypeRouter
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http':get_asgi_application()
+}
+)
