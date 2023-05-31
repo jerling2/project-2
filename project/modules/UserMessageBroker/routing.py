02c2aa2033844eb7cdb4566a96bd05f626332b8e
Joseph Erlinger <josepherlinger@gmail.com>
2023-05-30 20:10:00 -0700

    changed name to user_message_broker.py

--- project/modules/UserMessageBroker/routing.py
+++ project/modules/UserMessageBroker/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from . import consumers
+from . import user_message_broker
 
 websocket_urlpatterns = [
-    re_path(r'ws/messagebroker/$', consumers.UserMessageBroker.as_asgi()),
+    re_path(r'ws/messagebroker/$', user_message_broker.UserMessageBroker.as_asgi()),
 ]
