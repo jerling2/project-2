a74d05f3c912cb93f34abfd9023994d4bcad41e6
Joseph Erlinger <josepherlinger@gmail.com>
2023-06-04 21:55:42 -0700

    implemented courses page

--- project/static/courses.js
+++ project/static/courses.js
@@ -1,19 +1,21 @@
-// Get the view buttons list.
-const view_button_node_list = document.querySelectorAll(".course-view-details-button");
+// ---------------------------------------------------------------------------- //
+// THIS IS OUTDATED AND SHOULD BE DELETED IF NOT USED IN FINISHED VERSION       //
+// ---------------------------------------------------------------------------- //
 
-/* Detect if the user clicks on 'view/hide details' and either display or hide the extra details. */
-view_button_node_list.forEach(view_button => {
-    view_button.addEventListener("click", () => {
-        const course_container = view_button.parentNode.parentNode; // go to parents up.
-        const details_container = course_container.querySelector(".course-shown-details-container");
-        if (details_container.classList.contains("hidden")) {
-            view_button.textContent = "hide details";
-            details_container.classList.remove("hidden");
-        } else {
-            view_button.textContent = "view details";
-            details_container.classList.add("hidden");
-        }
+// // Get the view buttons list.
+// const view_button_node_list = document.querySelectorAll(".course-view-details-button");
 
-        console.log(details_container);
-    });
-});
+// /* Detect if the user clicks on 'view/hide details' and either display or hide the extra details. */
+// view_button_node_list.forEach(view_button => {
+//     view_button.addEventListener("click", () => {
+//         const course_container = view_button.parentNode.parentNode; // go to parents up.
+//         const details_container = course_container.querySelector(".course-shown-details-container");
+//         if (details_container.classList.contains("hidden")) {
+//             view_button.textContent = "hide details";
+//             details_container.classList.remove("hidden");
+//         } else {
+//             view_button.textContent = "view details";
+//             details_container.classList.add("hidden");
+//         }
+//     });
+// });
